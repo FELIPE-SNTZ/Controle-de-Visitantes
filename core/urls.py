e521032b048e2ca0a6b15c4a20f1656b822e0d95
FELIPE-SNTZ <ruan4036@gmail.com>
2024-10-07 17:38:19 -0300

    feat:Criação de butão de autorizar entrada e finalizar

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
-from dashboard.views import index
+from apps.visitantes.views import finalizar_visita
+from apps.dashboard.views import index
 from django.contrib import admin
 from django.urls import path
-from visitantes.views import registrar_visitante, informacoes_visitante
+from apps.visitantes.views import registrar_visitante, informacoes_visitante
 from django.contrib.auth import views as auth_views
-from visitantes.views import informacoes_visitante
+from apps.visitantes.views import informacoes_visitante
 
 
 urlpatterns = [
@@ -11,6 +12,8 @@ urlpatterns = [
     path('',index,name='index'),
     path('registro',registrar_visitante,name='registrar_visitante'),
     path('login/', auth_views.LoginView.as_view(template_name = 'login.html'), name='login'),
-    path('visitante/<int:pk>', informacoes_visitante,name='informacoes_visitante')
+    path('visitante/<int:pk>', informacoes_visitante,name='informacoes_visitante'),
+    path('visitante/<int:pk>/finalizar-visita', finalizar_visita,name='finalizar_visita')
+
 
 ]
