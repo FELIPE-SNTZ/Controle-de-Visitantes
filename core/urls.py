52fd8fc9bd0c4f4b5b851733eae498349770d0a5
FELIPE-SNTZ <ruan4036@gmail.com>
2024-10-08 15:49:56 -0300

    feat:Criação de butões de sair e voltar

--- core/urls.py
+++ core/urls.py
@@ -1,17 +1,20 @@
-from apps.visitantes.views import finalizar_visita
 from apps.dashboard.views import index
 from django.contrib import admin
 from django.urls import path
-from apps.visitantes.views import registrar_visitante, informacoes_visitante
-from django.contrib.auth import views as auth_views
-from apps.visitantes.views import informacoes_visitante
+from apps.visitantes.views import registrar_visitante, informacoes_visitante,finalizar_visita
+from django.contrib.auth.views import LoginView,LogoutView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',index,name='index'),
+
+    
+    path('login/',LoginView.as_view(template_name = 'login.html'), name='login'),
+    path('logout/',LogoutView.as_view(template_name = 'logout.html'), name='logout'),
+
+
     path('registro',registrar_visitante,name='registrar_visitante'),
-    path('login/', auth_views.LoginView.as_view(template_name = 'login.html'), name='login'),
     path('visitante/<int:pk>', informacoes_visitante,name='informacoes_visitante'),
     path('visitante/<int:pk>/finalizar-visita', finalizar_visita,name='finalizar_visita')
 
